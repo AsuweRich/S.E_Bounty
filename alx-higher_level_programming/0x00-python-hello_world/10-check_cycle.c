4a7d27ae1d31b5a9a8632f2a05d24e2b9a632686
Asuwe Richard Wuese <pickhimyoung24@yahoo.com>
2023-04-13 13:18:37 +0100

    higher level programming

---
+++ alx-higher_level_programming/0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: pointer to the head of the list
+ * Return: 0 in case of no cycle and 1 at the opposite
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *p2;
+	listint_t *prev;
+
+	p2 = list;
+	prev = list;
+	while (list && p2 && p2->next)
+	{
+		list = list->next;
+		p2 = p2->next->next;
+
+		if (list == p2)
+		{
+			list = prev;
+			prev = p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
+
+				list = list->next;
+			}
+			return (1);
+		}
+	}
+
+	return (0);
+}
