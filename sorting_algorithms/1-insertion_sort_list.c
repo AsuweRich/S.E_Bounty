74a10bcb9899d2acf0effcf663414afbc9ace49f
Asuwe Richard Wuese <pickhimyoung24@yahoo.com>
2023-04-29 13:11:50 +0100

    Sorting algorithms

---
+++ sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+{
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
+}
+
+
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of
+ * integers in ascending order
+ * @list: A pointer to the head of the doubly linked list
+ *
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *iter, *insert, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	{
+		tmp = iter->next;
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
+		{
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
+		}
+
+	}
+
+
+}
---
+++ sorting_algorithms/sort.h
@@ -0,0 +1,48 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Comparison direction macros for bitonic sort*/
+#define UP 0
+#define DOWN 1
+
+/*enum bool - Enumeration of boolean values*/
+typedef enum bool
+{
+    false = 0,
+    true
+} bool;
+
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/*Helper for swapping*/
+void swap_ints(int *a, int *b);
+
+/*Printing helper functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/*Regular functions prototypes*/
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+
+
+#endif
